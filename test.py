426226b6a1eabda09e6b3a044d1b8c0c771464ac
neno1978 <titinoytitina.marcob@me.com>
2015-11-19 22:17:42 +0100

    Update test.py

--- test.py
+++ test.py
@@ -3,8 +3,11 @@ xbmc.executebuiltin( "XBMC.Action(back)" )
 time.sleep(3)
 timeout = time.time() + 3
 while time.time() < timeout:
+      test = 0
+    if test == 5 or time.time() > timeout:
       xbmc.executebuiltin( "Container.Refresh" )
-print "container refrescado"      
+      break
+     
       
       
       
