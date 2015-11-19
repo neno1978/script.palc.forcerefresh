1aa211872314358d36c1ffdc8ebf558e564df8b6
neno1978 <titinoytitina.marcob@me.com>
2015-11-19 22:59:32 +0100

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,10 @@ import xbmc, time
 xbmc.executebuiltin( "XBMC.Action(back)" )
 time.sleep(3)
 xbmc.executebuiltin( "Container.Refresh" )
+pepe=xbmc.executebuiltin( "Container.Refresh" )
+while pepe:
+      print "ooooooo"
+print "uuuuuuuu"      
 
      
       
