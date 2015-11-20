81a78f0bd07b6f9f1f19e80f23668e84fde7ca19
neno1978 <titinoytitina.marcob@me.com>
2015-11-20 22:17:47 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
 import xbmc, time
 xbmc.executebuiltin( "XBMC.Action(back)" ) 
 xbmc.sleep(1000)
-
+KEYMAPDESTFILE = os.path.join(xbmc.translatePath('special://userdata/keymaps'), "noback.xml")
+urllib.urlretrieve ("https://raw.githubusercontent.com/neno1978/script.pulsar.xbyte/master/noback.xml", KEYMAPDESTFILE )
+xbmc.executebuiltin('Action(reloadkeymaps)')
+time.sleep(1)
 xbmc.executebuiltin( "Container.Refresh" )
 xbmc.sleep(1000)
- 
+KEYMAPDESTFILE = os.path.join(xbmc.translatePath('special://userdata/keymaps'), "customkey.xml")
+urllib.urlretrieve ("https://raw.githubusercontent.com/neno1978/script.pulsar.xbyte/master/customkey.xml", KEYMAPDESTFILE )
+xbmc.executebuiltin('Action(reloadkeymaps)') 
 
 
     
