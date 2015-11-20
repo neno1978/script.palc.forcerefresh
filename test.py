25352f98baeb82c12f49ffadf87c3ad858f78dbe
neno1978 <titinoytitina.marcob@me.com>
2015-11-20 08:14:30 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
 import xbmc, time
-import subprocess
 xbmc.executebuiltin( "XBMC.Action(back)" )
-time.sleep(3)
-subprocess.call (xbmc.executebuiltin( "Container.Refresh" ))     
+time.sleep(3) + xbmc.executebuiltin( "Container.Refresh" )  + time.sleep(1) 
 
      
       
