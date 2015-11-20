eb4b17de5a8a1e077ed3b8d427411942261dfd2d
neno1978 <titinoytitina.marcob@me.com>
2015-11-20 23:31:31 +0100

    Update test.py

--- test.py
+++ test.py
@@ -3,14 +3,17 @@ import urlparse,urllib2,urllib,re
 import os, sys
 xbmc.executebuiltin( "XBMC.Action(back)" ) 
 xbmc.sleep(1000)
-KEYMAPDESTFILE = os.path.join(xbmc.translatePath('special://userdata/keymaps'), "noback.xml")
-urllib.urlretrieve ("https://raw.githubusercontent.com/neno1978/script.pulsar.xbyte/master/noback.xml", KEYMAPDESTFILE )
+NOBACKDESTFILE = os.path.join(xbmc.translatePath('special://userdata/keymaps'), "noback.xml")
+CUSTOMKEYDESTFILE = os.path.join(xbmc.translatePath('special://userdata/keymaps'), "customkey.xml")
+os.remove(CUSTOMKEYDESTFILE)
+print "ck boraddo"
+urllib.urlretrieve ("https://raw.githubusercontent.com/neno1978/script.pulsar.xbyte/master/noback.xml", NOBACKDESTFILE )
 xbmc.executebuiltin('Action(reloadkeymaps)')
 time.sleep(1)
 xbmc.executebuiltin( "Container.Refresh" )
 xbmc.sleep(1000)
-KEYMAPDESTFILE = os.path.join(xbmc.translatePath('special://userdata/keymaps'), "customkey.xml")
-urllib.urlretrieve ("https://raw.githubusercontent.com/neno1978/script.pulsar.xbyte/master/customkey.xml", KEYMAPDESTFILE )
+os.remove(NOBACKDESTFILE)
+urllib.urlretrieve ("https://raw.githubusercontent.com/neno1978/script.pulsar.xbyte/master/customkey.xml", CUSTOMKEYDESTFILE )
 xbmc.executebuiltin('Action(reloadkeymaps)') 
 
 
