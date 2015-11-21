680cb9cb633855979daf87d1928686dc2b626bf1
neno1978 <titinoytitina.marcob@me.com>
2015-11-21 09:54:20 +0100

    Update test.py

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ urllib.urlretrieve ("https://raw.githubusercontent.com/neno1978/script.pulsar.xb
 xbmc.executebuiltin('Action(reloadkeymaps)')
 time.sleep(1)
 xbmc.executebuiltin( "Container.Refresh" )
-time.sleep(2)
+time.sleep(3)
 try:
    os.remove(NOBACKDESTFILE)
 except:
