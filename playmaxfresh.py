96eddf4afa60babc2db6fa09b70c7b0e8e2041bb
neno1978 <titinoytitina.marcob@me.com>
2015-11-24 22:35:08 +0100

    Update playmaxfresh.py

--- playmaxfresh.py
+++ playmaxfresh.py
@@ -1,14 +1,11 @@
 import xbmc, time
 import urlparse,urllib2,urllib,re
 import os, sys
-
-xbmc.executebuiltin( "XBMC.Action(back)" )
-xbmc.sleep(200)
 NOBACKDESTFILE = os.path.join(xbmc.translatePath('special://userdata/keymaps'), "noback.xml")
 CUSTOMKEYDESTFILE = os.path.join(xbmc.translatePath('special://userdata/keymaps'), "customkey.xml")
-
 urllib.urlretrieve ("https://raw.githubusercontent.com/neno1978/script.pulsar.xbyte/master/noback.xml", NOBACKDESTFILE )
 xbmc.executebuiltin('Action(reloadkeymaps)')
+
 time.sleep(2)
 xbmc.executebuiltin( "Container.Refresh" )
 time.sleep(3)
