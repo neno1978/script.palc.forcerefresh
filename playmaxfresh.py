12b0e493fcc4af3e6cdcdc7b4381751b85e5b5ca
neno1978 <titinoytitina.marcob@me.com>
2015-11-24 22:32:44 +0100

    Update playmaxfresh.py

--- playmaxfresh.py
+++ playmaxfresh.py
@@ -9,7 +9,7 @@ CUSTOMKEYDESTFILE = os.path.join(xbmc.translatePath('special://userdata/keymaps'
 
 urllib.urlretrieve ("https://raw.githubusercontent.com/neno1978/script.pulsar.xbyte/master/noback.xml", NOBACKDESTFILE )
 xbmc.executebuiltin('Action(reloadkeymaps)')
-print "Container Refrescado"
+time.sleep(2)
 xbmc.executebuiltin( "Container.Refresh" )
 time.sleep(3)
 try:
