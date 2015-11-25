1871775bdcd713f5fd1face39ffbb2d3fd7a388c
neno1978 <titinoytitina.marcob@me.com>
2015-11-25 06:52:25 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,12 @@
 import xbmc, time
 import urlparse,urllib2,urllib,re
 import os, sys
-
-xbmc.executebuiltin( "XBMC.Action(back)" )
-time.sleep(2)
 NOBACKDESTFILE = os.path.join(xbmc.translatePath('special://userdata/keymaps'), "noback.xml")
 CUSTOMKEYDESTFILE = os.path.join(xbmc.translatePath('special://userdata/keymaps'), "customkey.xml")
-
 urllib.urlretrieve ("https://raw.githubusercontent.com/neno1978/script.pulsar.xbyte/master/noback.xml", NOBACKDESTFILE )
 xbmc.executebuiltin('Action(reloadkeymaps)')
-print "Container Refrescado"
+time.sleep(1)
+xbmc.executebuiltin( "XBMC.Action(back)" )
 xbmc.executebuiltin( "Container.Refresh" )
 time.sleep(5)
 try:
