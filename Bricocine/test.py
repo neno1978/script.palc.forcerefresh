9557da0897ad5f475093b2b4e54f5d0ec90afa6b
neno1978 <titinoytitina.marcob@me.com>
2015-11-26 21:51:10 +0100

    Update test.py

--- Bricocine/test.py
+++ Bricocine/test.py
@@ -3,8 +3,12 @@ import urlparse,urllib2,urllib,re
 import os, sys
 NOBACKDESTFILE = os.path.join(xbmc.translatePath('special://userdata/keymaps'), "noback.xml")
 REMOTENOBACKDESTFILE = os.path.join(xbmc.translatePath('special://userdata/keymaps'), "remotenoback.xml")
+APPCNOBACKDESTFILE = os.path.join(xbmc.translatePath('special://userdata/keymaps'), "appcnoback.xml")
+TOUCHDNOBACKESTFILE = os.path.join(xbmc.translatePath('special://userdata/keymaps'), "touchnoback.xml")
 urllib.urlretrieve ("https://raw.githubusercontent.com/neno1978/script.palc.forcerefresh/master/noback.xml", NOBACKDESTFILE )
 urllib.urlretrieve ("https://raw.githubusercontent.com/neno1978/script.palc.forcerefresh/master/Bricocine/remotenoback.xml", REMOTENOBACKDESTFILE)
+urllib.urlretrieve ("https://raw.githubusercontent.com/neno1978/script.palc.forcerefresh/master/appcnoback.xml", APPCNOBACKDESTFILE )
+urllib.urlretrieve ("https://raw.githubusercontent.com/neno1978/script.palc.forcerefresh/master/Bricocine/touchnoback.xml", TOUCHNOBACKDESTFILE)
 xbmc.executebuiltin('Action(reloadkeymaps)')
 xbmc.executebuiltin( "XBMC.Action(back)" )
 time.sleep(1)
@@ -13,6 +17,8 @@ time.sleep(3)
 try:
    os.remove(NOBACKDESTFILE)
    os.remove(REMOTENOBACKDESTFILE)
+   os.remove(APPCNOBACKDESTFILE)
+   os.remove(TOUCHNOBACKDESTFILE)
 except:
    pass    
 
