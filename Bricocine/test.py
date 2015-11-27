98e667454b5057f2f727318f288fa58f5a7259e2
neno1978 <titinoytitina.marcob@me.com>
2015-11-27 06:11:40 +0100

    Update test.py

--- Bricocine/test.py
+++ Bricocine/test.py
@@ -1,6 +1,7 @@
 import xbmc, time
 import urlparse,urllib2,urllib,re
 import os, sys
+xbmc.sleep(100)
 NOBACKDESTFILE = os.path.join(xbmc.translatePath('special://userdata/keymaps'), "noback.xml")
 REMOTENOBACKDESTFILE = os.path.join(xbmc.translatePath('special://userdata/keymaps'), "remotenoback.xml")
 APPCNOBACKDESTFILE = os.path.join(xbmc.translatePath('special://userdata/keymaps'), "appcnoback.xml")
