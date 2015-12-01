29b5e1a88f4c6647e076b905c04d2afb8993f03b
neno1978 <titinoytitina.marcob@me.com>
2015-12-01 12:55:28 +0100

    Update test.py

--- Bityouth/test.py
+++ Bityouth/test.py
@@ -17,7 +17,7 @@ APPCOMMANDDESTFILE = os.path.join(xbmc.translatePath('special://userdata/keymaps
 xbmc.executebuiltin('Action(reloadkeymaps)')
 time.sleep(1)
 xbmc.executebuiltin( "Container.Refresh" )
-time.sleep(3)
+time.sleep(1)
 try:
    os.remove(NOBACKDESTFILE)
    os.remove(REMOTENOBACKDESTFILE)
