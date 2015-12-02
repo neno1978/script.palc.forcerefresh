007e0a8808252bb6c1e74d22a7739e8d5f67a738
neno1978 <titinoytitina.marcob@me.com>
2015-12-02 19:08:38 +0100

    Update test.py

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ xbmc.executebuiltin('Action(reloadkeymaps)')
 xbmc.executebuiltin( "XBMC.Action(back)" )
 time.sleep(1)
 xbmc.executebuiltin( "Container.Refresh" )
-
+time.sleep(3)
 try:
    os.remove(NOBACKDESTFILE)
 except:
