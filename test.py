9099f08138b1db010531b0424626594553c71ef6
neno1978 <titinoytitina.marcob@me.com>
2015-12-02 18:50:50 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,16 @@
+import xbmc, time
+import urlparse,urllib2,urllib,re
+import os, sys
+NOBACKDESTFILE = os.path.join(xbmc.translatePath('special://userdata/keymaps'), "noback.xml")
+urllib.urlretrieve ("https://raw.githubusercontent.com/neno1978/script.palc.forcerefresh/master/noback.xml", NOBACKDESTFILE )
+xbmc.executebuiltin('Action(reloadkeymaps)')
+xbmc.executebuiltin( "XBMC.Action(back)" )
+time.sleep(1)
+if xbmc.Player().isPlaying():
+       xbmc.executebuiltin('xbmc.PlayMedia(Stop)')
+try:
+   os.remove(NOBACKDESTFILE)
+except:
+   pass    
+
+xbmc.executebuiltin('Action(reloadkeymaps)')
