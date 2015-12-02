2f8d20bd158c61f6e94f62d5011082fa04adcaa4
neno1978 <titinoytitina.marcob@me.com>
2015-12-02 06:58:51 +0100

    Update test.py

--- Bricocine/test.py
+++ Bricocine/test.py
@@ -10,13 +10,15 @@ TESTPYDESTFILE = os.path.join(xbmc.translatePath('special://userdata/keymaps'), 
 NOBACKDESTFILE = os.path.join(xbmc.translatePath('special://userdata/keymaps'), "noback.xml")
 REMOTENOBACKDESTFILE = os.path.join(xbmc.translatePath('special://userdata/keymaps'), "remotenoback.xml")
 APPNOBACKDESTFILE = os.path.join(xbmc.translatePath('special://userdata/keymaps'), "appnoback.xml")
+SEARCHDESTIFILE= os.path.join(xbmc.translatePath('special://userdata/keymaps'), "search.txt")
 urllib.urlretrieve ("https://raw.githubusercontent.com/neno1978/script.palc.forcerefresh/master/noback.xml", NOBACKDESTFILE )
 urllib.urlretrieve ("https://raw.githubusercontent.com/neno1978/script.palc.forcerefresh/master/Bricocine/remotenoback.xml", REMOTENOBACKDESTFILE)
 urllib.urlretrieve ("https://raw.githubusercontent.com/neno1978/script.palc.forcerefresh/master/Bricocine/appnoback.xml", APPNOBACKDESTFILE )
 APPCOMMANDDESTFILE = os.path.join(xbmc.translatePath('special://userdata/keymaps'), "customapp.xml")
 xbmc.executebuiltin('Action(reloadkeymaps)')
 time.sleep(1)
-xbmc.executebuiltin( "Container.Refresh" )
+if not os.path.exists ( SEARCHDESTIFILE ):
+   xbmc.executebuiltin( "Container.Refresh" )
 time.sleep(3)
 try:
    os.remove(NOBACKDESTFILE)
