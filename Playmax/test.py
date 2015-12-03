a3260bb078efb7c8ee916b187bd3ebeddd014814
neno1978 <titinoytitina.marcob@me.com>
2015-12-03 22:35:07 +0100

    Update test.py

--- Playmax/test.py
+++ Playmax/test.py
@@ -1,6 +1,7 @@
 import xbmc, time
 import urlparse,urllib2,urllib,re
 import os, sys
+xbmc.executebuiltin( "XBMC.Action(back)" )
 NOBACKDESTFILE = os.path.join(xbmc.translatePath('special://userdata/keymaps'), "noback.xml")
 urllib.urlretrieve ("https://raw.githubusercontent.com/neno1978/script.palc.forcerefresh/master/noback.xml", NOBACKDESTFILE )
 xbmc.executebuiltin('Action(reloadkeymaps)')
